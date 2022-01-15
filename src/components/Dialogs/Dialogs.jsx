762602bee921bca5120f2dc00db41e00e37bdbbd
Taras <81424893+tar1kkk@users.noreply.github.com>
2022-01-15 18:49:53 +0200

    Dialogs-item

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,25 +1,32 @@
 import { NavLink } from 'react-router-dom';
 import './Dialogs.css';
 
+const DialogsItem = (props) => {
+	let path = '/dialogs/' + props.id;
+	return <div className='item active'>
+		<NavLink to={path}>{props.name}</NavLink>
+	</div>
+}
+const DialogsMessage = (props) => {
+	return <div className="message">{props.message}</div>
+}
+
+
 const Dialogs = (props) => {
 	return (
 		<div className='content'>
 			<div className='dialogs'>
 				<div className="dialogs-item">
-					<div className='item active'>
-						<NavLink to='/dialogs/1'>Sasha</NavLink>
-					</div>
-					<div className='item'>
-						<NavLink to='/dialogs/2'>Misha</NavLink>
-					</div>
-					<div className='item'>
-						<NavLink to='/dialogs/3'>Koval4yk</NavLink>
-					</div>
+					<DialogsItem name='Sasha' id='1' />
+					<DialogsItem name='Misha' id='2' />
+					<DialogsItem name='Andey' id='3' />
 				</div>
 				<div className="messages">
-					<div className="message">Hi</div>
-					<div className="message">React developer?</div>
-					<div className="message">yo</div>
+					<DialogsMessage message='Hi' />
+					<DialogsMessage message='My name is Taras' />
+					<DialogsMessage message='Yo' />
+					<DialogsMessage message='Yo' />
+					<DialogsMessage message='Yo' />
 				</div>
 			</div>
 		</div>
--- src/components/Profile/myposts/MyPosts.jsx
+++ src/components/Profile/myposts/MyPosts.jsx
@@ -1,15 +1,16 @@
 import Post from "./post/post";
+import './MyPosts.css';
+
 
 const MyPosts = () => {
-	return <div>
-		<div>My posts</div>
-
-		<div><textarea></textarea>
-			<button>add post</button></div>
+	return <div className="myPosts">
+		<div className="title">My posts</div>
+		<textarea className="txt" id="" cols="35" rows="2"></textarea>
+		<div><button className="button">Add post</button></div>
 		<div className="post">
 			<Post message='Hi,how are u?' />
 			<Post message="It's my first post" />
 		</div>
-	</div>
+	</div >
 }
 export default MyPosts;
